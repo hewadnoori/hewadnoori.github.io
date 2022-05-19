637a8a14fca4945a6798d242ec995bb082cb551b
Hewad Noori <hewadnoori16@gmail.com>
2022-05-18 22:05:25 -0400

    done

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,12 +1,26 @@
-window.addEventListener('load', function () {
-    if (window.matchMedia('screen and (max-width: 1200px)').matches) {
+const nav = document.querySelector('nav');
+const navButton = nav.firstElementChild.firstElementChild.nextElementSibling;
+const navContent = navButton.previousElementSibling.firstElementChild;
 
-        console.log('hello')
+window.addEventListener('load', function () {
+    if (screen.width >= 992) {
+        navButton.style.display = 'none';//removes the nav button
+        navContent.classList.remove('collapse');//shows the nav content by removing the collapse class which is hiding it.
     }
-    nav = document.querySelector('nav');
-    navButton = nav.firstElementChild.firstElementChild.nextElementSibling;
-    navContent = navButton.previousElementSibling.firstElementChild;
-    console.log(navContent);
-    navButton.remove()
-    navContent.classList.remove('collapse')
-})
+    if (screen.width < 992) {
+        navButton.style.display = 'block';
+        navContent.classList.add('collapse');
+    }
+
+    window.addEventListener('resize', function () {
+        if (screen.width >= 992) {
+            navButton.style.display = 'none';//removes the nav button
+            navContent.classList.remove('collapse');//shows the nav content by removing the collapse class which is hiding it.
+        }
+        if (screen.width < 992) {
+            navButton.style.display = 'block';
+            navContent.classList.add('collapse');
+        }
+    });
+
+});
